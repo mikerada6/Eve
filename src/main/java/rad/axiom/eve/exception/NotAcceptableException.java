0a5dc2115bd7365302b95ef22431c3ac28fcd194
Mike Rad <mrada6@gmail.com>
2020-12-30 15:00:28 -0500

    Create new Exception to allow for better communication with Wall-E

---
+++ src/main/java/rad/axiom/eve/exception/NotAcceptableException.java
@@ -0,0 +1,16 @@
+package rad.axiom.eve.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_ACCEPTABLE) public class NotAcceptableException  extends RuntimeException {
+
+    public NotAcceptableException() {
+
+    }
+
+    public NotAcceptableException(String message) {
+        super(message);
+    }
+}
+
